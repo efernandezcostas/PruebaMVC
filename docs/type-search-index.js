50c84c173c4dd82ac9d7b3893106c04b502c0db0
efernandezcostas <efernandezcostas@danielcastelao.org>
2024-05-06 11:25:06 +0200

    docs pa prueba

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Coche"},{"p":"<Unnamed>","l":"Model"},{"p":"<Unnamed>","l":"ModelTest"}];updateSearchResults();
